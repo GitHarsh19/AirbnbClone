e2d7c93995be9088ed6d77d47eb34c0de8c0ff3f
harshit <harshitagarwalsmt@gmail.com>
2023-09-02 01:17:41 +0530

    lecture-8

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,15 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
+import axios from 'axios';
 
 export default class extends Controller {
-  static targets = ['email', 'submit'];
+  static targets = ['email','submit'];
   connect () {
-    this.submitTarget.addEventListener('click', (e) => {
-      e.preventDefault();
-      if(this.emailTarget.value.length === 0){
-        
-      } else {
+      
+      this.submitTarget.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(this.emailTarget.value.length === 0 ) {
 
-      }
-    });
-  }  
+        } else {
+          axios.get('/api/users_by_emails', {
+            params: {
+              email: this.emailTarget.value
+            },
+            headers: {
+               'ACCEPT': 'application/json'
+            }
+          }).then((response)=> {
+            Turbo.visit('/users/sign_in')
+          }).catch((response) => {
+            Turbo.visit('/users/sign_up')
+          })
+        }
+      });
+  }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
+pin "axios", to: "https://cdn.skypack.dev/axios@1.2.0"
