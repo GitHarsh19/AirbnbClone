882dd20a54ded677b25fc61f5de2453c29ed5306
harshit <harshitagarwalsmt@gmail.com>
2023-09-05 20:36:02 +0530

    lecture-15

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
 pin "axios", to: "https://cdn.skypack.dev/axios@1.2.0"
+pin "geolib", to: "https://ga.jspm.io/npm:geolib@3.3.4/es/index.js"
